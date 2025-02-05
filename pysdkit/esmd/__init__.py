8eabfd8894554ade1f1772bbefd0d1f392bc15e9
wwhenxuan <wwhenxuan@gmail.com>
2025-02-05 21:55:32 +0800

    update markdown

---
+++ pysdkit/esmd/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2025/02/05 21:04:23
+@author: Whenxuan Wang
+@email: wwhenxuan@gmail.com
+"""
---
+++ pysdkit/esmd/esmd.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2025/02/05 21:04:29
+@author: Whenxuan Wang
+@email: wwhenxuan@gmail.com
+"""
+import numpy as np
+
+
+class ESMD(object):
+
+    def __init__(self):
+        pass
