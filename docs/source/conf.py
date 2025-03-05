b3978483bdb96005c32ba3fe471a4cf4bd85b908
changewam <3133986068@qq.com>
2025-03-05 22:31:45 +0800

    try

---
+++ docs/source/conf.py
@@ -0,0 +1,123 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+from datetime import date
+import os
+import sys
+from pathlib import Path
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "PYSDKIT"
+
+copyright = f"2025-{date.today().year}, the pysdkit team"
+author = "the pysdkit team"
+
+release = "0.4.15"
+version = "0.4.15"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+#sys.path.append(str(Path('exts').resolve()))
+
+extensions = [
+    "sphinx_copybutton",
+    "sphinx.ext.duration",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode",
+    "sphinx_design",
+
+    "sphinx_gitstamp"
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+source_suffix = {".rst": "restructuredtext"}
+#__________________________________________________________________________
+html_logo = "_static/logo.png"
+
+language = "zh_CN"
+
+show_warning_types = True
+suppress_warnings = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "pydata_sphinx_theme"
+html_static_path = ["_static"]
+
+html_css_files = ["theme_overrides.css","custom.css"]
+
+html_theme_options = {
+    "announcement": "",#You can specify an arbitrary URL that will be used as the HTML source for your announcement. 
+    # Navigation bar
+    #_____________________________________________________________
+    "logo": {
+        "text": "pysdkit",
+        "link": "",
+    },
+    "header_links_before_dropdown":4 ,
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/wwhenxuan/PySDKit",
+            "icon": "fa-brands fa-github",
+            "type": "fontawesome",
+        },
+        {
+            "name": "PyPI",
+            "url": "https://pypi.org/project/PySDKit/",
+            "icon": "_static/logo-pypi.svg",
+            "type": "url",
+        },
+    ],
+    "navbar_align": "content",
+    "navbar_start": ["navbar-logo","version-switcher"],
+    "navbar_center": ["navbar-nav"],
+    #"navbar_end": ["navbar-icon-links"], 
+    #"navbar_persistent": ["search-button"],
+    #______________________________________________________________________________________
+    "switcher": {
+        "json_url": (""),#the persistent location of the JSON file
+        "version_match": "dev" if "dev" in version else version,
+    },
+    "show_version_warning_banner": True,
+    # Secondary_sidebar_items
+    "secondary_sidebar_items": {
+    "**": ["page-toc", "sourcelink"],
+    "index": [],
+    },
+    # Footer
+    "footer_start": ["copyright"],
+    "footer_end": ["sphinx-version", "theme-version"],
+    # Color
+   "pygments_light_style": "xcode",
+   "pygments_dark_style": "monokai",
+    # Other
+    "show_prev_next": False,
+    "show_nav_level": 1,
+    "back_to_top_button": True,
+    
+    #"use_edit_page_button": True,
+
+
+}
+
+remove_from_toctrees = []
+
+# Custom sidebar templates, maps document names to template names.
+html_sidebars = {
+    "index": []  # Hide sidebar in home page
+}
+html_show_sourcelink = False
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = "scikitimagedoc"
---
+++ docs/source/index.rst
@@ -0,0 +1,81 @@
+.. PYSDKIT documentation master file, created by
+   sphinx-quickstart on Sun Feb 23 11:49:25 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+PYSDKIT's documentation
+===================================
+
+**Version**: |version|,**Date**: 
+.. gitstamp:: 
+
+Quick links
+-----------
+.. grid:: 2
+    :gutter: 3
+
+    .. grid-item-card:: :octicon:`rocket` Get started
+        :link: user_guide/install
+        :link-type: any
+
+        New to pysdkit? Start with our installation guide and pysdkit's key
+        concepts.
+
+    .. grid-item-card:: :octicon:`image` Examples
+        :link: examples/index
+        :link-type: any
+
+        Browse our gallery of entry-level and domain-specific examples.
+
+    .. grid-item-card:: :octicon:`comment-discussion` Get help :octicon:`link-external`
+        :link: https://forum.image.sc/tag/scikit-image
+
+        Need help with a particular image analysis task? Ask us and a large
+        image analysis community on our user forum *image.sc*.
+
+    .. grid-item-card:: :octicon:`repo` API reference
+        :link: api/api
+        :link-type: any
+
+        A detailed description of scikit-image's public Python API. Assumes an
+        understanding of the key concepts.
+
+    .. grid-item-card:: :octicon:`tools` Contribute
+        :link: development/contribute
+        :link-type: any
+
+        Saw a typo? Found a bug? Want to improve a function? Learn how to
+        contribute to scikit-image!
+
+    .. grid-item-card:: :octicon:`history` Release notes
+        :link: release_notes/index
+        :link-type: any
+
+        Upgrading from a previous version? See what's new and changed between
+        each release of scikit-image.
+
+
+
+
+
+.. toctree::
+   :hidden:
+
+   user_guide/index
+   examples/index
+   API reference/index
+   release_notes/index
+   development/index
+   about/index
+
+
+
+
+
+
+
+
+
+
+
+
