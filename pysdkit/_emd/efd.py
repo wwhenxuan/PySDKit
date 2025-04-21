57951a410968b589329971430d2da176eccedb16
wwhenxuan <wwhenxuan@gmail.com>
2025-04-21 23:34:39 +0800

    Update EFD

--- pysdkit/_emd/efd.py
+++ pysdkit/_emd/efd.py
@@ -11,69 +11,6 @@ from typing import Optional, Tuple
 from pysdkit.utils import fft, ifft, fmirror
 
 
-def segm_tec(f: np.ndarray, N: Optional[int] = 3) -> Tuple[np.ndarray, float]:
-    """
-    This function is used to implement the improved segmentation technique.
-    function [boundaries,cerf] = Boundaries_Dect(ff,N)
-
-    :param f: the Fourier spectrum of input signal
-    :param N: maximum number of segments
-    :return: -cerf: vector containing the central frequency of each band,[0,pi]
-             -bound: vector containing the set of boundaries corresponding
-             to the Fourier line segmentation (normalized between 0 and Pi)
-    """
-    bounds, cerf = None, None
-
-    # detect the local maxima and minina
-    locmax = np.zeros(f.shape)
-    locmin = np.max(f) * np.ones(f.shape)
-
-    # 遍历整个序列寻找局部极大值和局部极小值
-    for i in range(1, len(f) - 1):
-        if f[i - 1] < f[i] and f[i] > f[i + 1]:
-            # 局部极大值
-            locmax[i] = f[i]
-        if f[i - 1] > f[i] and f[i] < f[i + 1]:
-            # 局部极小值
-            locmin[i] = f[i]
-
-    # 处理最后的边缘
-    locmax[0] = f[0]
-    locmax[-1] = f[-1]
-
-    # keep the N-th highest maxima and their index
-    if N != 0:
-        lmax = np.sort(locmax)[::-1]  # 按列降序排序
-        Imax = np.argsort(locmax)[::-1]  # 获取排序后的索引
-
-        if len(lmax) > N:
-            Imax = np.sort(Imax[0:N])
-        else:
-            Imax = np.sort(Imax)
-            N = len(lmax)
-
-        # detect the lowest minima between two consecutive maxima
-        M = N + 1  # numbers of the boundaries
-        omega = np.concatenate([np.array([0]), Imax, np.array([len(f)])])  # location
-
-        bounds = np.zeros(M)
-
-        for i in range(0, M):
-            if (i == 0 or i == M - 1) and omega[i] == omega[i + 1]:
-                bounds[i] = omega[i]
-            else:
-                # print(omega[i], omega[i + 1])
-                lmin = np.min(f[omega[i] : omega[i + 1]])
-                ind = np.argmin(f[omega[i] : omega[i + 1]])
-                print("ind", ind)
-                bounds[i] = omega[i] + ind
-            cerf = Imax * np.pi / np.round(len(f))
-
-    print("bounds", bounds)
-
-    return bounds, cerf
-
-
 class EFD(object):
     """
     Empirical Fourier Decomposition
@@ -94,29 +31,29 @@ class EFD(object):
     def __init__(self, max_imfs: Optional[int] = 3) -> None:
         self.max_imfs = max_imfs
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self, signal: np.ndarray, return_all: Optional[bool] = False) -> np.ndarray | Tuple[np.ndarray, float]:
         """allow instances to be called like functions"""
-        pass
+        return self.fit_transform(signal=signal, return_all=return_all)
 
     def __str__(self) -> str:
         """Get the full name and abbreviation of the algorithm"""
         return "Empirical Fourier Decomposition (EFD)"
 
-    def fit_transform(self, signal: np.ndarray) -> np.ndarray:
+    def fit_transform(self, signal: np.ndarray, return_all: Optional[bool] = False) -> np.ndarray | Tuple[np.ndarray, float]:
         """
         Signal decomposition using EFD algorithm
 
         :param signal: the time domain signal (1D numpy array) to be decomposed
+        :param return_all: whether to return all results (including vector containing the central frequency) or just the IMFs
         :return: the decomposed results of IMFs
         """
-        # 确保输入信号长度为偶数否则镜像拓展无法进行
+        # Make sure the input signal length is an even number, otherwise the mirror expansion cannot be performed.
         if len(signal) % 2 == 1:
             signal = signal[:-1]
 
-        print("signal", len(signal))
-
         # We compute the Fourier transform of input signal
         ff = fft(signal)
+
         # We extract the boundaries of Fourier segments
         bounds, cerf = segm_tec(
             f=np.abs(ff[0 : int(np.round(len(ff) / 2))]), N=self.max_imfs
@@ -128,23 +65,26 @@ class EFD(object):
         # Filtering
         # We extend the signal by miroring to deal with the boundaries
         x = fmirror(ts=signal, sym=len(signal) // 2)
+
+        # Half the record length is used to recover the original signal
         T = len(x) // 2
 
+        # Perform fast Fourier transform on the image-extended signal
         ff = fft(x)
-        print("len(ff)", len(ff))
 
         # We obtain the boundaries in the extend f
         bound2 = np.ceil(bounds * np.round(len(ff) / 2) / np.pi).astype(int)
-        print("bounds: ", bounds)
-        print("bound2", bound2)   # TODO: 这里大了1
 
         # We get the core of filtering
         number = len(bound2) - 1
+
+        # Record the basic results after decomposition
         efd = np.zeros(shape=(number, len(ff)))
+
+        # Record the final results
         imfs = np.zeros(shape=(number, len(signal)))
 
-        print("efd.shape", efd.shape)
-
+        # The results are stored in the frequency domain
         ft = np.zeros([number, len(ff)])
 
         # We define an ideal functions and extract components
@@ -156,10 +96,72 @@ class EFD(object):
                 ft[k, bound2[k]: bound2[k + 1]] = ff[bound2[k]: bound2[k + 1]]
                 ft[k, len(ff) + 2 - bound2[k + 1]: len(ff) + 2 - bound2[k]] = ff[len(ff) + 2 - bound2[k + 1]: len(ff) + 2 - bound2[k]]
 
+            # Recover the original signal from the frequency domain
             efd[k, :] = np.real(ifft(ft[k, :]))
             imfs[k, :] = efd[k][T // 2: 3 * T // 2]
 
+        if return_all is True:
+            return imfs, cerf
         return imfs
+
+
+def segm_tec(f: np.ndarray, N: Optional[int] = 3) -> Tuple[np.ndarray, float]:
+    """
+    This function is used to implement the improved segmentation technique.
+    function [boundaries,cerf] = Boundaries_Dect(ff,N)
+
+    :param f: the Fourier spectrum of input signal
+    :param N: maximum number of segments
+    :return: -cerf: vector containing the central frequency of each band,[0,pi]
+             -bound: vector containing the set of boundaries corresponding
+             to the Fourier line segmentation (normalized between 0 and Pi)
+    """
+    bounds, cerf = None, None
+
+    # detect the local maxima and minina
+    locmax = np.zeros(f.shape)
+    locmin = np.max(f) * np.ones(f.shape)
+
+    # Traverse the entire sequence to find local maxima and local minima
+    for i in range(1, len(f) - 1):
+        if f[i - 1] < f[i] and f[i] > f[i + 1]:
+            # Local maximum
+            locmax[i] = f[i]
+        if f[i - 1] > f[i] and f[i] < f[i + 1]:
+            # Local Minima
+            locmin[i] = f[i]
+
+    # Processing the final edge
+    locmax[0] = f[0]
+    locmax[-1] = f[-1]
+
+    # keep the N-th highest maxima and their index
+    if N != 0:
+        lmax = np.sort(locmax)[::-1]  # Sort by column in descending order
+        Imax = np.argsort(locmax)[::-1]  # Get the sorted index
+
+        if len(lmax) > N:
+            Imax = np.sort(Imax[0:N])
+        else:
+            Imax = np.sort(Imax)
+            N = len(lmax)
+
+        # detect the lowest minima between two consecutive maxima
+        M = N + 1  # numbers of the boundaries
+        omega = np.concatenate([np.array([0]), Imax, np.array([len(f)])])  # location
+        bounds = np.zeros(M)
+
+        for i in range(0, M):
+            if (i == 0 or i == M - 1) and omega[i] == omega[i + 1]:
+                bounds[i] = omega[i]
+            else:
+                # print(omega[i], omega[i + 1])
+                lmin = np.min(f[omega[i] : omega[i + 1]])
+                ind = np.argmin(f[omega[i] : omega[i + 1]])
+                bounds[i] = omega[i] + ind
+            cerf = Imax * np.pi / np.round(len(f))
+
+    return bounds, cerf
 
 
 
