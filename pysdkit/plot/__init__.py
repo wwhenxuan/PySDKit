7c1eb31eabf9c091caacc29223d4e828a92e901c
wwhenxuan <wwhenxuan@gmail.com>
2025-05-01 22:44:03 +0800

    Update init for plotting

--- pysdkit/plot/__init__.py
+++ pysdkit/plot/__init__.py
@@ -5,20 +5,19 @@ Created on Sat Mar 4 21:31:05 2024
 @email: wwhenxuan@gmail.com
 Some auxiliary function modules for data visualization in the PySDKit library
 """
-# 在二维平面上可视化原输入信号和分解得到的本征模态函数
+# Visualize the original input signal and the decomposed IMFs on a 2D plane
 from ._plot_imfs import plot_IMFs
 
-# 单独绘制分解得到的每个本征模态函数的频谱
+# Plot the spectrum of each decomposed IMF separately
 from ._fourier_spectra import plot_IMFs_amplitude_spectra
 
-# 绘制希尔伯特谱
+# Plotting the Hilbert spectrum
 from ._fourier_spectra import plot_HilbertSpectrum
 
-# 用于可视化二维灰度图
+# Used to visualize two-dimensional grayscale images
 from ._plot_images import plot_grayscale_image, plot_grayscale_spectrum
 
-# 通用的图像可视化函数
-# 可以选择是否绘制频域图谱以及是否添加颜色条
+# General image visualization functions
 from ._plot_images import plot_images
 
 # Functions that generate signal visualizations
