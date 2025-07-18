453329aa0f6ab222dc72965063ce76bd32916045
wwhenxuan <wwhenxuan@gmail.com>
2025-07-18 14:41:36 +0800

    whenxuan: create the folder of unittest for data generation

--- pysdkit/tests/data/test_generator_signal.py
+++ pysdkit/tests/data/test_generator_signal.py
@@ -21,31 +21,33 @@ from pysdkit.data import (
 
 
 class Base_Signal_Generator_Test(unittest.TestCase):
-    """验证各种生成基础形式信号生成算法的测试模块"""
+    """Test modules for verifying various basic signal generation algorithms"""
 
     duration = 1.0
     sample_rate = 1000
     noise_level = 0.0
 
     def test_add_noise(self) -> None:
-        """验证添加噪声的函数"""
+        """Verify the function of adding noise"""
         for N in [10, 100, 500, 1000]:
-            # 遍历不同的信号长度
+            # Traversing different signal lengths
             for mean in [0.0, 0.1, -0.1, 0.5, -0.5, 5, 10, 20, 100, -100]:
-                # 遍历不同的均值
+                # Iterate over different means
                 for std in [0.1, 0.5, 1, 2, 3, 5]:
-                    # 遍历不同的标准差
+                    # Iterate through different standard deviations
                     y = add_noise(N, mean, std)
-                    self.assertTrue(expr=len(y) == N, msg="输出长度错误")
-                    # 验证噪声均值的数值差异
+                    self.assertTrue(expr=len(y) == N, msg="Output length error")
+
+                    # Validate the numerical difference in noise means
                     diff_mean = np.allclose(np.mean(y), mean, atol=1e-6)
-                    self.assertTrue(expr=diff_mean, msg="输出噪声的均值不符合要求")
-                    # 验证噪声标准差的数值差异
+                    self.assertTrue(expr=diff_mean, msg="The mean value of the output noise does not meet the requirements")
+
+                    # Verify the numerical difference in noise standard deviation
                     diff_std = np.allclose(np.std(y), std, atol=1e-6)
-                    self.assertTrue(expr=diff_std, msg="输出噪声的标准差不符合要求")
+                    self.assertTrue(expr=diff_std, msg="The standard deviation of the output noise does not meet the requirements")
 
     def test_generate(self) -> None:
-        """测试所有生成信号的函数"""
+        """Test all functions that generate signals"""
         for fun in [
             generate_cos_signal,
             generate_sin_signal,
@@ -55,110 +57,121 @@ class Base_Signal_Generator_Test(unittest.TestCase):
             generate_am_signal,
             generate_exponential_signal,
         ]:
-            # 生成各种形式的信号
+            # Generate various signals
             time, signal = fun(
                 duration=self.duration,
                 sampling_rate=self.sample_rate,
                 noise_level=self.noise_level,
             )
-            # 判断信号长度和时间戳数组的长度是否符合要求
+            # Determine whether the signal length and timestamp array length meet the requirements
             self.assertTrue(
-                expr=len(signal) == self.sample_rate, msg="信号的长度出现错误"
+                expr=len(signal) == self.sample_rate, msg="The length of the signal is wrong"
             )
             self.assertTrue(
-                expr=len(time) == self.sample_rate, msg="时间错长度出现错误"
+                expr=len(time) == self.sample_rate, msg="Time error length error"
             )
-            # 判断时间戳数组和信号长度是否匹配
+
+            # Determine whether the timestamp array and signal length match
             self.assertTrue(
-                expr=len(signal) == len(time), msg="信号的长度和时间戳的长度不匹配"
+                expr=len(signal) == len(time), msg="The length of the signal and the length of the timestamp do not match"
             )
 
     def test_sine_generator(self) -> None:
-        """测试正弦信号的生成"""
+        """Test sinusoidal signal generation"""
         for frequency in [1, 5, 10, 20]:
-            # 遍历不同的时间频率生成信号
+            # Iterate over different time frequencies to generate signals
             time, signal = generate_sin_signal(
                 duration=self.duration,
                 sampling_rate=self.sample_rate,
                 noise_level=self.noise_level,
                 frequency=frequency,
             )
-            # 生成指定的信号
+
+            # Generates the specified signal
             sine = np.sin(2 * np.pi * frequency * time)
-            # 判断生成信号和真实信号的差异
+
+            # Determine the difference between the generated signal and the real signal
             diff = np.allclose(sine, signal, atol=1e-6)
-            self.assertTrue(expr=diff, msg="生成的正弦信号存在数值错误")
+
+            self.assertTrue(expr=diff, msg="The generated sinusoidal signal has numerical errors")
 
     def test_cosine_generator(self) -> None:
-        """测试余弦信号的生成"""
+        """Test cosine signal generation"""
         for frequency in [1, 5, 10, 20]:
-            # 遍历不同的时间频率生成信号
+            # Iterate over different time frequencies to generate signals
             time, signal = generate_cos_signal(
                 duration=self.duration,
                 sampling_rate=self.sample_rate,
                 noise_level=self.noise_level,
                 frequency=frequency,
             )
-            # 生成指定的余弦信号
+            # Generates the specified cosine signal
             cosine = np.cos(2 * np.pi * frequency * time)
-            # 判断生成信号和真实信号的差异
+            # Determine the difference between the generated signal and the real signal
             diff = np.allclose(cosine, signal, atol=1e-6)
-            self.assertTrue(expr=diff, msg="生成的余弦信号存在数值错误")
+            self.assertTrue(expr=diff, msg="The generated cosine signal has numerical errors")
 
     def test_square_wave(self) -> None:
-        """验证方波信号的生成"""
+        """Verify square wave signal generation"""
         for frequency in [1, 5, 10, 20]:
-            # 遍历不同的时间频率生成信号
+            # Iterate over different time frequencies to generate signals
             time, signal = generate_square_wave(
                 duration=self.duration,
                 sampling_rate=self.sample_rate,
                 noise_level=self.noise_level,
                 frequency=frequency,
             )
-            # 生成指定的方波信号
+
+            # Generates the specified square wave signal
             square = np.sign(np.sin(2 * np.pi * frequency * time))
-            # 判断生成信号和真实信号的差异
+
+            # Determine the difference between the generated signal and the real signal
             diff = np.allclose(square, signal, atol=1e-6)
-            self.assertTrue(expr=diff, msg="生成的方波信号存在数值错误")
+            self.assertTrue(expr=diff, msg="The generated square wave signal has numerical errors")
 
     def test_triangle_wave(self) -> None:
-        """验证三角波信号的生成"""
+        """Verify the generation of triangle wave signal"""
         for frequency in [1, 5, 10, 20]:
-            # 遍历不同的时间频率生成信号
+            # Iterate over different time frequencies to generate signals
             time, signal = generate_triangle_wave(
                 duration=self.duration,
                 sampling_rate=self.sample_rate,
                 noise_level=self.noise_level,
                 frequency=frequency,
             )
-            # 生成指定的信号
+
+            # Generates the specified signal
             triangle = 2 * np.abs(sawtooth(2 * np.pi * frequency * time, 0.5)) - 1
-            # 判断生成信号和真实信号的差异
+
+            # Determine the difference between the generated signal and the real signal
             diff = np.allclose(triangle, signal, atol=1e-6)
-            self.assertTrue(expr=diff, msg="生成的三角波信号存在数值错误")
+
+            self.assertTrue(expr=diff, msg="The generated triangle wave signal has numerical errors")
 
     def test_sawtooth_wave(self) -> None:
-        """验证sawtooth信号的生成"""
+        """Verify sawtooth signal generation"""
         for frequency in [1, 5, 10, 20]:
-            # 遍历不同的时间频率生成信号
+            # Iterate over different time frequencies to generate signals
             time, signal = generate_sawtooth_wave(
                 duration=self.duration,
                 sampling_rate=self.sample_rate,
                 noise_level=self.noise_level,
                 frequency=frequency,
             )
-            # 生成指定的信号
+
+            # Generates the specified signal
             sawtooth_wave = sawtooth(2 * np.pi * frequency * time)
-            # 判断生成信号和真实信号的差异
+
+            # Determine the difference between the generated signal and the real signal
             diff = np.allclose(sawtooth_wave, signal, atol=1e-6)
-            self.assertTrue(expr=diff, msg="生成的sawtooth信号存在数值错误")
+            self.assertTrue(expr=diff, msg="The generated sawtooth signal has numerical errors")
 
     def test_exponential_signal(self) -> None:
-        """验证指数信号的生成"""
+        """Generation of validation index signals"""
         for decay_rate in [1.0, 2.0, -1.0, -2.0]:
-            # 遍历不同大小的指数衰减因子
+            # Iterate through exponential decay factors of different sizes
             for initial_amplitude in [1.0, 2.0, -1.0, -2.0]:
-                # 遍历不同大小的常数因子
+                # Iterate over constant factors of different sizes
                 time, signal = generate_exponential_signal(
                     duration=self.duration,
                     sampling_rate=self.sample_rate,
@@ -166,18 +179,20 @@ class Base_Signal_Generator_Test(unittest.TestCase):
                     initial_amplitude=initial_amplitude,
                     noise_level=self.noise_level,
                 )
-                # 生成指定的信号
+
+                # Generates the specified signal
                 exp = initial_amplitude * np.exp(-decay_rate * time)
-                # 判断生成信号和真实信号的差异
+
+                # Determine the difference between the generated signal and the real signal
                 diff = np.allclose(exp, signal, atol=1e-6)
-                self.assertTrue(expr=diff, msg="生成的指数信号存在数值错误")
+                self.assertTrue(expr=diff, msg="The generated exponential signal has a numerical error")
 
     def test_am_signal(self) -> None:
-        """验证Amplitude Modulated (AM)信号的生成"""
+        """Verify Amplitude Modulated (AM) signal generation"""
         for carrier_freq in [100, 120, 130, 150]:
             for modulating_freq in [5, 10, 20]:
                 for mod_index in [1, 2]:
-                    # 遍历不同的时间频率生成信号
+                    # Iterate over different time frequencies to generate signals
                     time, signal = generate_am_signal(
                         duration=self.duration,
                         sampling_rate=self.sample_rate,
@@ -186,17 +201,20 @@ class Base_Signal_Generator_Test(unittest.TestCase):
                         modulating_freq=modulating_freq,
                         mod_index=mod_index,
                     )
-                    # 生成指定的信号
+
                     # Carrier signal
                     carrier = np.cos(2 * np.pi * carrier_freq * time)
+
                     # Modulating signal
                     modulating_signal = np.cos(2 * np.pi * modulating_freq * time)
+
                     # Amplitude Modulated signal
                     am_signal = (1 + mod_index * modulating_signal) * carrier
-                    # 判断生成信号和真实信号的差异
+
+                    # Determine the difference between the generated signal and the real signal
                     diff = np.allclose(am_signal, signal, atol=1e-6)
                     self.assertTrue(
-                        expr=diff, msg="生成的Amplitude Modulated (AM)信号存在数值错误"
+                        expr=diff, msg="The generated Amplitude Modulated (AM) signal has numerical errors"
                     )
 
 
